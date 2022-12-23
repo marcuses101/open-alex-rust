88074c8a06c4b66f34af2d54700c777d79c0e53a
Marcus Connolly <mnjconnolly@gmail.com>
2022-12-23 13:08:46 -0500

    TEMP

---
+++ src/lib.rs
@@ -0,0 +1,160 @@
+#[allow(dead_code)]
+use csv;
+use eyre::Result;
+use reqwest::{header::USER_AGENT, Client};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Deserialize, Debug)]
+pub struct ConceptCountByYear {
+    year: u32,
+    works_count: u32,
+    cited_by_count: u32,
+}
+#[derive(Deserialize, Debug)]
+pub struct OpenAlexMeta {
+    count: u32,
+    db_response_time_ms: u32,
+    page: u32,
+    per_page: u32,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct OpenAlexResponse<T> {
+    meta: OpenAlexMeta,
+    results: Vec<T>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct IdsObject {
+    openalex: String,
+    wikidata: Option<String>,
+    mag: Option<String>,
+    wikipedia: Option<String>,
+    umls_aui: Option<Vec<String>>,
+    umls_cui: Option<Vec<String>>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct ConceptAncestor {
+    id: String,
+    wikidata: Option<String>,
+    display_name: String,
+    level: u32,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct RelatedConcept {
+    id: String,
+    wikidata: Option<String>,
+    display_name: String,
+    level: u32,
+    score: f32,
+}
+#[derive(Deserialize, Debug)]
+pub struct International {
+    display_name: HashMap<String, String>,
+}
+#[derive(Deserialize, Debug)]
+pub struct Concept {
+    pub id: String,
+    // wikidata: Option<String>,
+    pub display_name: String,
+    pub level: u32,
+    pub description: Option<String>,
+    // works_count: u32,
+    // cited_by_count: u32,
+    // ids: Ids_Object,
+    // image_url: Option<String>,
+    // image_thumbnail_url: Option<String>,
+    // international: International,
+    ancestors: Vec<ConceptAncestor>,
+    // related_concepts: Vec<RelatedConcept>,
+    pub counts_by_year: Vec<ConceptCountByYear>,
+    pub works_api_url: String,
+    // updated_date: String,
+    // created_date: String,
+}
+
+#[derive(Debug, Serialize)]
+pub struct CSVRecord<'a> {
+    id: &'a String,
+    level: &'a u32,
+    display_name: &'a String,
+    description: &'a Option<String>,
+    works_api_url: &'a String,
+    year: u32,
+    works_count: u32,
+    cited_by_count: u32,
+}
+
+async fn get_concept_page(
+    client: &Client,
+    page: u32,
+    level: &u32,
+) -> Result<OpenAlexResponse<Concept>> {
+    let per_page: u32 = 200;
+    let path = format!(
+        "https://api.openalex.org/concepts?filter=level:{level}&per-page={per_page}&page={page}"
+    );
+    let response = client
+        .get(path)
+        .header(USER_AGENT, "mailto:mnjconnolly@gmail.com")
+        .send()
+        .await?;
+    let json = response.json::<OpenAlexResponse<Concept>>().await?;
+    Ok(json)
+}
+
+pub async fn get_concepts(client: Client, level: &u32) -> Result<Vec<Concept>> {
+    println!("fetching page 1 of ??? of level: {level}");
+    let first_response = get_concept_page(&client, 1, &level).await?;
+    let max_results = first_response.meta.count;
+    let last_page = max_results / first_response.meta.per_page + 1;
+    let mut results = first_response.results;
+    for current_page in 2..=last_page {
+        println!("fetching page {current_page} of {last_page}");
+        let response_result = get_concept_page(&client, current_page, &level).await;
+        match response_result {
+            Ok(res) => res
+                .results
+                .into_iter()
+                .for_each(|concept| results.push(concept)),
+            Err(_) => {
+                println!("failed to fetch page {}", current_page)
+            }
+        }
+    }
+    Ok(results)
+}
+
+pub async fn get_all_level_concepts(Client: Client) -> Result<Vec<Concept>> {
+    let concepts: Vec<Concept> = vec![];
+    (0..=5).into_iter().for_each(|level: u32| async {})
+    Ok(concepts)
+}
+
+pub fn write_concepts_to_csv_file(concepts: Vec<Concept>, level: &u32) -> Result<()> {
+    let filename = format!("level_{level}_concepts.csv");
+    println!("writing to \"{filename}\"");
+    let mut csv_writer = csv::Writer::from_path(filename)?;
+    for concept in concepts.iter() {
+        concept
+            .counts_by_year
+            .iter()
+            .for_each(|count_per_year_entry| {
+                let record = CSVRecord {
+                    id: &concept.id,
+                    level: &concept.level,
+                    works_api_url: &concept.works_api_url,
+                    display_name: &concept.display_name,
+                    description: &concept.description,
+                    cited_by_count: count_per_year_entry.cited_by_count,
+                    works_count: count_per_year_entry.works_count,
+                    year: count_per_year_entry.year,
+                };
+                csv_writer.serialize(record).expect("unable to serialize");
+            })
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,30 @@
-use reqwest::header::USER_AGENT;
-use serde::Deserialize;
-use std::collections::HashMap;
-
-#[derive(Deserialize, Debug)]
-struct ConceptCountByYear {
-    year: u32,
-    works_count: u32,
-    cited_by_count: u32,
-}
-#[derive(Deserialize, Debug)]
-struct Open_Alex_Meta {
-    count: u32,
-    db_response_time_ms: u32,
-    page: u32,
-    per_page: u32,
-}
-
-#[derive(Deserialize, Debug)]
-struct Open_Alex_Response<T> {
-    meta: Open_Alex_Meta,
-    results: Vec<T>,
-}
-
-#[derive(Deserialize, Debug)]
-struct Ids_Object {
-    openalex: String,
-    wikidata: Option<String>,
-    mag: Option<String>,
-    wikipedia: Option<String>,
-    umls_aui: Option<Vec<String>>,
-    umls_cui: Option<Vec<String>>,
-}
-
-#[derive(Deserialize, Debug)]
-struct Concept_Ancestor {
-    id: String,
-    wikidata: Option<String>,
-    display_name: String,
+use eyre::Result;
+use open_alex_rust::Concept;
+use std::env;
+use std::process;
+struct Config {
     level: u32,
 }
 
-#[derive(Deserialize, Debug)]
-struct RelatedConcept {
-    id: String,
-    wikidata: Option<String>,
-    display_name: String,
-    level: u32,
-    score: f32,
-}
-#[derive(Deserialize, Debug)]
-struct International {
-    display_name: HashMap<String, String>,
-}
-#[derive(Deserialize, Debug)]
-struct Concept {
-    id: String,
-    // wikidata: Option<String>,
-    display_name: String,
-    // level: u32,
-    description: Option<String>,
-    // works_count: u32,
-    // cited_by_count: u32,
-    // ids: Ids_Object,
-    // image_url: Option<String>,
-    // image_thumbnail_url: Option<String>,
-    // international: International,
-    // ancestors: Vec<Concept_Ancestor>,
-    // related_concepts: Vec<RelatedConcept>,
-    counts_by_year: Vec<ConceptCountByYear>,
-    // works_api_url: String,
-    // updated_date: String,
-    // created_date: String,
+impl Config {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config> {
+        args.next();
+        let level: u32 = args.next().unwrap().parse()?;
+        Ok(Config { level })
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let open_alex_base_path = "https://api.openalex.org/concepts?filter=level:5";
+    let config = Config::build(env::args()).unwrap_or_else(|er| {
+        eprintln!("Problem parsing arguments: {er}");
+        process::exit(1);
+    });
+    let level = &config.level;
     let client = reqwest::Client::new();
-    let response = client
-        .get(open_alex_base_path)
-        .header(USER_AGENT, "mailto:mnjconnolly@gmail.com")
-        .send()
-        .await?;
-    // println!("{:#?}", response);
-    let json = response.json::<Open_Alex_Response<Concept>>().await?;
-    //   println!("{:#?}", text);
-    // let json: serde_json::Value = serde_json::from_str(&text)?;
-    // let json: Open_Alex_Response<Concept> = serde_json::from_str(&text)?;
-    // let body = response.json::<Open_Alex_Response<Concept>>().await?;
-    println!("{:#?}", json);
+    let concepts: Vec<Concept> = vec![];
+
+    // let concepts = open_alex_rust::get_concepts(client, level).await?;
+    open_alex_rust::write_concepts_to_csv_file(concepts, level)?;
     Ok(())
 }
